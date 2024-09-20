ab727832c838682572504c6a0d45f7b9328e80ab
wtg42 <weiting.shi1982@gmail.com>
2024-09-21 00:45:29 +0800

    feat: 建構基礎架構

--- tui/load_eml.go
+++ tui/load_eml.go
@@ -2,7 +2,6 @@
 package tui
 
 import (
-	"fmt"
 	"log"
 	"os"
 
@@ -22,55 +21,36 @@ const (
 )
 
 func (m EmlModel) Init() tea.Cmd {
-	return m.filepicker.Init()
+	return nil
 }
 
 func (m EmlModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
 	switch m.status {
 	case isFilepicker:
-		log.Printf("jjjjjjj=>%d", m.status)
 		switch msg := msg.(type) {
 		case tea.KeyMsg:
 			switch msg.String() {
 			case "ctrl+c", "q", "esc":
 				return m, tea.Quit
 			}
-			log.Println("fffff")
+			m.filepicker, cmd = m.filepicker.Update(msg)
 			// Did the user select a file?
 			if didSelect, path := m.filepicker.DidSelectFile(msg); didSelect {
-				log.Println("Selected file:", path)
 				// Get the path of the selected file.
 				m.selectedFile = path
 			}
-			m.filepicker, cmd = m.filepicker.Update(msg)
 			return m, cmd
 		}
-
-		{
-			// 私有的 Msg Type 只能靠字串分析
-			isFilePickerReadDirMsg := fmt.Sprintf("%T", msg)
-			if isFilePickerReadDirMsg == "filepicker.readDirMsg" {
-				log.Println("llllll", isFilePickerReadDirMsg)
-				var fpCmd tea.Cmd
-				m.filepicker, fpCmd = m.filepicker.Update(msg)
-
-				// Did the user select a file?
-				if didSelect, path := m.filepicker.DidSelectFile(msg); didSelect {
-					log.Println("Selected file:", path)
-					// Get the path of the selected file.
-					m.selectedFile = path
-				}
-				return m, fpCmd
-			}
-		}
-		return m, nil
+		// The other msg behavior still needs to update the filepicker.
+		m.filepicker, cmd = m.filepicker.Update(msg)
+		return m, cmd
 	}
 	return m, nil
 }
 
 func (m EmlModel) View() string {
-	pickfileDscription := "\nPick a file: "
+	pickfileDscription := "\nPick a .eml file: "
 	if m.selectedFile != "" {
 		pickfileDscription = "\nSelected file: " + m.filepicker.Styles.Selected.Render(m.selectedFile)
 	}
