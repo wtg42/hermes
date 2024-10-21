dfe13ccd2f7513662e8dbdece9547c0b1ca6e126
wtg42 <weiting.shi1982@gmail.com>
2024-10-21 15:23:28 +0800

    fix(burst mode): Fix the issue where the email header is written into the Bcc field.

--- tui/mail_burst.go
+++ tui/mail_burst.go
@@ -94,7 +94,7 @@ func (m MailBurstModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		switch msg := msg.(type) {
 		case tea.KeyMsg:
 			switch msg.String() {
-			case "ctrl+c", "q":
+			case "ctrl+c":
 				return m, tea.Quit
 			case "tab":
 				m.session = receiverDomainInput
